8975aa5f60add3c4803cbf7613f4d688b98e7dea
Remyy112 <remigiuszpawlowski05@gmail.com>
2022-11-02 10:13:26 +0100

    zapis1

--- zapytania.sql
+++ zapytania.sql
@@ -6,3 +6,12 @@
 6. SELECT `customer_name` FROM `customers` ORDER BY `country` ASC
 7. SELECT `customer_name` FROM `customers` ORDER BY `country` DESC
 8. SELECT `customer_name` FROM `customers` ORDER BY `country` DESC, `customer_name` ASC
+
+1. SELECT MIN(price) FROM `products` 
+2. SELECT MAX(price) FROM `products`
+3. SELECT COUNT(*) FROM `products`
+4. SELECT COUNT(*) FROM `customers`
+5. SELECT COUNT(*) FROM `customers` GROUP BY `country`
+6. SELECT COUNT(*) FROM `products` WHERE `category_id`=1
+7. SELECT COUNT(*) FROM `products` WHERE `category_id`=1
+8. SELECT MAX(price) FROM `products` WHERE `category_id`=1 OR `category_id`=2 GROUP BY `category_id`
