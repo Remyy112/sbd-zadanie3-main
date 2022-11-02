80ed68822ce72ce523140b885b2e75b3508095e6
Remyy112 <remigiuszpawlowski05@gmail.com>
2022-11-02 12:07:39 +0100

    zapis1

--- zapytania.sql
+++ zapytania.sql
@@ -15,3 +15,12 @@
 6. SELECT COUNT(*) FROM `products` WHERE `category_id`=1
 7. SELECT COUNT(*) FROM `products` WHERE `category_id`=1
 8. SELECT MAX(price) FROM `products` WHERE `category_id`=1 OR `category_id`=2 GROUP BY `category_id`
+
+
+1. SELECT name FROM `products` WHERE price BETWEEN 10 AND 20
+2. SELECT name FROM `products` WHERE price NOT BETWEEN 10 AND 20
+3. SELECT name FROM `products` WHERE `category_id` IN (1,2,3)
+4. SELECT * FROM `orders` WHERE `shipper_id` IN (1,2,3)
+5. SELECT name FROM `products` WHERE price BETWEEN 10 AND 15 AND `category_id` NOT IN (1,2,3,4)
+6. SELECT * FROM orders WHERE time(order_date) BETWEEN '1996-07-01' AND '1996-07-31';
+7. SELECT * FROM `orders` WHERE customer_id IN (SELECT customer_id FROM orders WHERE customer_id BETWEEN 77 AND 90)
